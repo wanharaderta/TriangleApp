ae119254898d85764d19c916bd2db2fd881d0202
wanhar <wanhar@sehatq.com>
2021-02-21 23:17:27 +0700

    clean code

--- TriangleAppTests/TriangleAppTests.swift
+++ TriangleAppTests/TriangleAppTests.swift
@@ -30,45 +30,61 @@ class TriangleAppTests: XCTestCase {
     }
   }
   
-  func testInvalidInputSides(){
-    XCTAssertThrowsError(try detectTriangle(-1, 2, 3)) { error in
+  func testInvalidInputSides() {
+    XCTAssertThrowsError(try detectTriangle(-1, -2, -3)) { error in
       XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
     }
   }
-  
-  
+
   func testDetectEquilateralTriangle() {
     XCTAssertEqual(try detectTriangle(2, 2, 2), "Segitiga Sama Sisi")
   }
- 
+
   func testDetectIsoscelesTriangle() {
-    XCTAssertEqual(try detectTriangle(1, 1, 2), "Segitiga Sama Kaki")
-    XCTAssertEqual(try detectTriangle(1, 2, 1), "Segitiga Sama Kaki")
-    XCTAssertEqual(try detectTriangle(1, 2, 2), "Segitiga Sama Kaki")
-    XCTAssertEqual(try detectTriangle(2, 1, 2), "Segitiga Sama Kaki")
+    XCTAssertEqual(try detectTriangle(8, 8, 10), "Segitiga Sama Kaki")
+    XCTAssertEqual(try detectTriangle(8, 10, 8), "Segitiga Sama Kaki")
+    XCTAssertEqual(try detectTriangle(8, 10, 10), "Segitiga Sama Kaki")
+    XCTAssertEqual(try detectTriangle(10, 8, 10), "Segitiga Sama Kaki")
+  }
+
+  func testDetectRandomTriangle() {
+    XCTAssertEqual(try detectTriangle(3, 4, 5), "Segitiga Sembarang")
   }
   
+  func testInequalityTriangle() {
+    XCTAssertThrowsError(try detectTriangle(4, 1, 2)) { error in
+      XCTAssertEqual(error as? TriangleError, TriangleError.inequalityInput)
+    }
+    XCTAssertThrowsError(try detectTriangle(5, 1, 3)) { error in
+      XCTAssertEqual(error as? TriangleError, TriangleError.inequalityInput)
+    }
+  }
+
   func detectTriangle(
     _ sideA: Int,
     _ sideB: Int,
     _ sideC: Int
   ) throws -> String {
-    for side in [sideA, sideB, sideC] {
-      if side < 1 {
+    let sides = [sideA, sideB, sideC].sorted()
+    for side in sides {
+      if side < 0 {
         throw TriangleError.invalidInput
       }
     }
-    
-    if sideA == sideB && sideA == sideC {
+    if sides[1] + sides[0] <= sides[2] {
+      throw TriangleError.inequalityInput
+    } else if sides[0] == sides[1] && sides[0] == sides[2] {
       return "Segitiga Sama Sisi"
-    } else if sideA == sideB || sideA == sideC || sideB == sideC {
+    } else if sides[0] == sides[1] || sides[1] == sides[2] {
       return "Segitiga Sama Kaki"
     } else {
       return "Segitiga Sembarang"
     }
   }
+
 }
 
 enum TriangleError: Error {
   case invalidInput
+  case inequalityInput
 }
